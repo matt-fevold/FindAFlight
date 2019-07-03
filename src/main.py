170c151824d99b8ba4040447d3a1bce8f2046440
Matthew Fevold <matt.fevold@gmail.com>
2019-07-02 20:34:48 -0500

    more functionalization

--- src/main.py
+++ src/main.py
@@ -7,25 +7,31 @@ with open('../resources/secrets.json') as secrets:
 with open('../resources/config.json') as config_file:
     data = json.load(config_file)
 
-
-def main():
+def call_api():
     # build search param and headers
     search = "msp/US/anytime?inboundpartialdate=2019-12-01"
-    header = {"X-RapidAPI-Host": data['API_HOST'],"X-RapidAPI-Key": secrets['API_KEY']}
+    header = {"X-RapidAPI-Host": data['API_HOST'], "X-RapidAPI-Key": secrets['API_KEY']}
 
     # make call
     response = requests.get(data['BASE_URL'] + search, headers=header)
 
     # validate response
-    print("RESPONSE CODE: ", response.status_code)
+    if response.status_code != 200:
+      print("RESPONSE CODE: ", response.status_code)
+      exit(1)
 
     # parse response into readable format
     quotes = response.json()['Quotes']
     places = response.json()['Places']
+    return quotes, places
+
+
+def main():
+    quotes, places = call_api()
 
     print(places)
 
-    write_destination_to_file(places)
+    # write_destination_to_file(places)
 
     cheap_flights = get_lower_than_price(quotes, 75)
 
